1351ffece7689b84a0e614fa4d278534ebd449e4
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-02-08 17:44:14 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/claudionogueira/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.claudionogueira.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.claudionogueira.cursomc.domain.Categoria;
+import com.claudionogueira.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
