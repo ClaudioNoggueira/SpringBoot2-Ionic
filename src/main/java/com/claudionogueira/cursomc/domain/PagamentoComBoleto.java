f06c6db3b80d44d98b3d7b050cc12ba4feef5251
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-02-11 17:16:58 -0300

    Mapeamento de herança Pagamento - PagamentoComBoleto/PagamentoComCartao

--- src/main/java/com/claudionogueira/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/claudionogueira/cursomc/domain/PagamentoComBoleto.java
@@ -2,8 +2,11 @@ package com.claudionogueira.cursomc.domain;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+
 import com.claudionogueira.cursomc.domain.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	private Date dataVencimento;
--- src/main/java/com/claudionogueira/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/claudionogueira/cursomc/domain/PagamentoComCartao.java
@@ -1,7 +1,10 @@
 package com.claudionogueira.cursomc.domain;
 
+import javax.persistence.Entity;
+
 import com.claudionogueira.cursomc.domain.enums.EstadoPagamento;
 
+@Entity
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	private Integer numeroDeParcelas;
