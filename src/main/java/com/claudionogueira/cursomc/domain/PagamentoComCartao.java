2a169bb18800e6a3176a92669aa3f05ad3279104
Claudio Nogueira <cllaudionoggueira@gmail.com>
2021-03-02 18:08:32 -0300

    Inserindo Pedido

--- src/main/java/com/claudionogueira/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/claudionogueira/cursomc/domain/PagamentoComCartao.java
@@ -3,8 +3,10 @@ package com.claudionogueira.cursomc.domain;
 import javax.persistence.Entity;
 
 import com.claudionogueira.cursomc.domain.enums.EstadoPagamento;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	private Integer numeroDeParcelas;
